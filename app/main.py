09c871835003fc677a039d549ff4e05f3d943123
Yehor Yurchenko <yegoryurtchenko05@gmail.com>
2025-03-11 22:39:02 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ import os
 
 class CleanUpFile:
 
-    def __init__(self, filename) -> None:
+    def __init__(self, filename: str) -> None:
         self.filename = filename
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
-        os.remove(self.filename)
-
+        try:
+            os.remove(self.filename)
+        except Exception as e:
+            print(f"Помилка під час видалення файлу: {e}")
