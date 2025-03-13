39d7846fdec6af85ff07cd10b59389bf33e689ec
Yehor Yurchenko <yegoryurtchenko05@gmail.com>
2025-03-13 17:24:39 +0200

    Solution7

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
-import os
+from __future__ import annotations
+from os import remove
+import traceback
 
 
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self):
+    def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> None:
-        if os.path.exists(self.filename):
-            os.remove(self.filename)
+    def __exit__(
+            self,
+            exc_type: type[BaseException] | None,
+            exc_val: BaseException | None,
+            exc_tb: traceback.TracebackType | None
+    ) -> None:
+        remove(self.filename)
